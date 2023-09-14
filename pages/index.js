5badbdf9da0aa8b4b62c6b04f292e52968cb7dce
LouiesAngelo <93696341+LouiesAngelo@users.noreply.github.com>
2023-09-14 10:46:25 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <title>No Talent Required</title>
       </Head>
       <section className={utilStyles.headingMd}>
-<h1>10 Things That Require Zero Talent</h1>
+<p>10 Things That Require Zero Talent</p>
     <ul>
   <li>1. Being On Time</li>
   <li>2. Making An Effort</li>

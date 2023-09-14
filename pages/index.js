0efb3c6915311578851e311b9069b34984cbc6f4
LouiesAngelo <93696341+LouiesAngelo@users.noreply.github.com>
2023-09-14 10:33:30 +0800

    index.js

--- pages/index.js
+++ pages/index.js
@@ -9,11 +9,6 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-    <ul>
-  <li>Coffee</li>
-  <li>Tea</li>
-  <li>Milk</li>
-</ul>
         <p>Greetings! I am Louies Angelo Mesia, commonly known as Gelo. the outgoing Vice-President (Internal) of the Student Society of Information Technology Education, outgoing PIO of the UA Central Student Council, and outgoing Features Editor of Regina- the UA Publication. Nonetheless, the incoming President of the Student Society of Information Technology Education, incoming Managing Editor of Regin, and Mr. UA-Scientia 2023.
 </p>
         <p>I am a socially inclined person wherein I enjoy interpersonal interaction. From that, I may have the capacity to be flexible and adaptable as with my leadership qualities with natural collaborative attributes. I have the ability to write, technical or creative, as I am a journalist since back then and a student leader who deals with letters, as well as I write poems, stories, and songs. Talking about songs,  I appreciate music as well and I am capable to play instruments such as guitar, piano, ukulele, and some basically. Probably, I got to mention that I do graphic designs, including publication layouts and logo making.</p>

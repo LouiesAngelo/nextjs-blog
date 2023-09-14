e1e2d27491b937476008b44d3a13a4b1aece1aa2
LouiesAngelo <93696341+LouiesAngelo@users.noreply.github.com>
2023-09-14 11:00:36 +0800

    index.js

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
       </Head> 
       <section className={utilStyles.headingMd}>
 <p>10 Things That Require ZeroTalent</p>
-    <ul>
+  <ul>
   <li>1. Being On Time</li>
   <li>2. Making An Effort</li>
   <li>3. Being High Energy</li>
@@ -21,7 +21,7 @@ export default function Home() {
   <li>8. Doing A Little Extra</li>
   <li>9. Being Prepared</li>
   <li>10. Having A Strong Work Ethic</li>
-    </ul>
+  </ul>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p> 

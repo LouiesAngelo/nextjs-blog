62742f841bfe68044c21679872b296e21dd076cc
LouiesAngelo <93696341+LouiesAngelo@users.noreply.github.com>
2023-09-14 10:41:11 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,19 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Greetings! I am Louies Angelo Mesia, commonly known as Gelo. the outgoing Vice-President (Internal) of the Student Society of Information Technology Education, outgoing PIO of the UA Central Student Council, and outgoing Features Editor of Regina- the UA Publication. Nonetheless, the incoming President of the Student Society of Information Technology Education, incoming Managing Editor of Regin, and Mr. UA-Scientia 2023.
+    <ul>
+  <li>1. Being On Time</li>
+  <li>2. Making An Effort</li>
+  <li>3. Being High Energy</li>
+  <li>4. Having A Positive Attitude</li>
+  <li>5. Being Passionate</li>
+  <li>6. Using Good Body Language</li>
+  <li>7. Being Coachable</li>
+  <li>8. Doing A Little Extra</li>
+  <li>9. Being Prepared</li>
+  <li>10. Having A Strong Work Ethic</li>
+</ul>
+      <p>Greetings! I am Louies Angelo Mesia, commonly known as Gelo. the outgoing Vice-President (Internal) of the Student Society of Information Technology Education, outgoing PIO of the UA Central Student Council, and outgoing Features Editor of Regina- the UA Publication. Nonetheless, the incoming President of the Student Society of Information Technology Education, incoming Managing Editor of Regin, and Mr. UA-Scientia 2023.
 </p>
         <p>I am a socially inclined person wherein I enjoy interpersonal interaction. From that, I may have the capacity to be flexible and adaptable as with my leadership qualities with natural collaborative attributes. I have the ability to write, technical or creative, as I am a journalist since back then and a student leader who deals with letters, as well as I write poems, stories, and songs. Talking about songs,  I appreciate music as well and I am capable to play instruments such as guitar, piano, ukulele, and some basically. Probably, I got to mention that I do graphic designs, including publication layouts and logo making.</p>
         <p>In my lifestyle, I am a spontaneous person. I like exploring, capturing moments, coffee shop hopping, and socializing with a bunch of people. In sum, in spite of my commitments, I also make space for personal stuff to live my life in harmony. All things matter, and for me everything is favorable.</p>
